47cf97a45754afc7cab240ad0950b16fe5a175ea
Merok23 <alejocalizblanco@gmail.com>
2023-03-21 14:40:45 -0300

    Arregle el estado, perdida de memoria, y si leia mal el archivo(por ahora)
     Committer: Alejo <alex@Alejo-Notebook>
     On branch main
     Changes to be committed:
    	new file:   TP 0 - Onboarding/bogus-code/input.txt
    	modified:   TP 0 - Onboarding/bogus-code/main.cpp
    	deleted:    TP 0 - Onboarding/bogus-code/main.cpp:Zone.Identifier
    	modified:   TP 0 - Onboarding/bogus-code/wordscounter.cpp

--- TP 0 - Onboarding/bogus-code/main.cpp
+++ TP 0 - Onboarding/bogus-code/main.cpp
@@ -10,11 +10,18 @@ int main(int argc, char* argv[]) {
     std::ifstream* input = new std::ifstream;
     if (argc > 1) {
         input->open(argv[1]);
+        if (((*input).rdstate() & std::ifstream::failbit ) != 0 ){
+            delete counter;
+            delete input;
+            return 1;
+        } 
         counter->process(*input);
     } else {
         counter->process(std::cin);
     }
     size_t words = counter->get_words();
     std::cout << words << std::endl;
+    delete counter;
+    delete input;
     return SUCCESS;
 }
